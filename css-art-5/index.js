02df96b171900c3917b80b9a1511e276c37a01bf
baran beyazıt <=>
2021-12-18 02:29:14 +0300

    main

---
+++ css-art-5/index.js
@@ -0,0 +1,128 @@
+
+
+
+
+let variable_facebook =  document.getElementById("facebook");
+let variable_twitter =  document.getElementById("twitter");
+let variable_instagram =  document.getElementById("instagram");
+let variable_github =  document.getElementById("github");
+let variable_youtube =  document.getElementById("youtube");
+
+
+let facebook1 = () => {
+let variable = variable_facebook.style;
+variable.visibility = "hidden"; 
+variable.padding = "10px";
+variable.transitionDuration = "0.5s";
+variable.fontSize="18px";
+variable.backgroundColor = "#3b5998";
+variable.position="absolute";
+variable.bottom="550px";
+variable.display="flex";
+}
+let facebook2 = () => {
+  let variable = variable_facebook.style;
+  variable.visibility = "visible";
+  variable.padding = "10px";
+  variable.transitionDuration = "0.5s";
+  variable.fontSize="18px";
+  variable.backgroundColor = "#3b5998";
+  variable.position="absolute";
+  variable.bottom="550px";
+  variable.display="flex";
+}
+
+
+
+
+let twitter1 = () => {
+  let variable = variable_twitter.style;
+  variable.visibility = "hidden"; 
+  variable.padding = "10px";
+  variable.transitionDuration = "0.5s";
+  variable.fontSize="18px";
+  variable.backgroundColor = "rgb(29, 155, 240";
+  variable.position="absolute";
+  variable.bottom="550px";
+  variable.display="flex";
+}
+let twitter2 = () => {
+  let variable = variable_twitter.style;
+  variable.visibility = "visible";
+  variable.padding = "10px";
+  variable.transitionDuration = "0.5s";
+  variable.fontSize="18px";
+  variable.backgroundColor = "rgb(29, 155, 240";
+  variable.position="absolute";
+  variable.bottom="550px";
+  variable.display="flex";
+}
+
+let instagram1 = () => {
+  let variable = variable_instagram.style;
+  variable.visibility = "hidden"; 
+  variable.padding = "10px";
+  variable.transitionDuration = "0.5s";
+  variable.fontSize="18px";
+  variable.backgroundColor = "#3f729b";
+  variable.position="absolute";
+  variable.bottom="550px";
+  variable.display="flex";
+}
+let instagram2 = () => {
+  let variable = variable_instagram.style;
+  variable.visibility = "visible";
+  variable.padding = "10px";
+  variable.transitionDuration = "0.5s";
+  variable.fontSize="18px";
+  variable.backgroundColor = "#3f729b";
+  variable.position="absolute";
+  variable.bottom="550px";
+  variable.display="flex";
+}
+
+let github1 = () => {
+  let variable = variable_github.style;
+  variable.visibility = "hidden"; 
+  variable.padding = "10px";
+  variable.transitionDuration = "0.5s";
+  variable.fontSize="18px";
+  variable.backgroundColor = "#7a7878";
+  variable.position="absolute";
+  variable.bottom="550px";
+  variable.display="flex";
+}
+let github2 = () => {
+  let variable = variable_github.style;
+  variable.visibility = "visible";
+  variable.padding = "10px";
+  variable.transitionDuration = "0.5s";
+  variable.fontSize="18px";
+  variable.backgroundColor = "#7a7878";
+  variable.position="absolute";
+  variable.bottom="550px";
+  variable.display="flex";
+}
+
+let youtube1 = () => {
+  let variable = variable_youtube.style;
+  variable.visibility = "hidden"; 
+  variable.padding = "10px";
+  variable.transitionDuration = "0.5s";
+  variable.fontSize="18px";
+  variable.backgroundColor = "#c4302b";
+  variable.position="absolute";
+  variable.bottom="550px";
+  variable.display="flex";
+}
+let youtube2 = () => {
+  let variable = variable_youtube.style;
+  variable.visibility = "visible";
+  variable.padding = "10px";
+  variable.transitionDuration = "0.5s";
+  variable.fontSize="18px";
+  variable.backgroundColor = "#c4302b";
+  variable.position="absolute";
+  variable.bottom="550px";
+  variable.display="flex";
+}
